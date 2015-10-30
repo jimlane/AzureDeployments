46ec08e530498e832b8cf5b1490e9f1bdaf8c6d3
Jim Lane <jimlane@microsoft.com>
2015-10-30 15:25:17 -0400

    Initial Push

---
+++ InterRoleCommunication/RoleWithInternalEndpoint/SenderFromCode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace RoleWithInternalEndpoint
+{
+    class SenderFromCode
+    {
+        IConversationChannel myChannel;
+        ChannelFactory<IConversationChannel> myFactory;
+        NetTcpBinding myBinding = new NetTcpBinding(SecurityMode.None, false);
+        public string myAddress { get; set; }
+
+        static void Main()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool ConfigComms()
+        {
+            try
+            {
+                EndpointAddress myAddr = new EndpointAddress(string.Format("net.tcp://{0}/", myAddress));
+
+                myFactory = new ChannelFactory<IConversationChannel>(myBinding, myAddr);
+                myChannel = myFactory.CreateChannel();
+                myChannel.Open(new TimeSpan(0, 2, 0));
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error configuring communication: " + ex.Message);
+                throw;
+            }
+
+        }
+
+        public bool SaySomething(string message)
+        {
+            if (myChannel.State == CommunicationState.Opened)
+            {
+                myChannel.Conversation(message);
+            }
+            return true;
+        }
+    }
+}
---
+++ InterRoleCommunication/RoleWithInternalEndpoint/WorkerRole.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace RoleWithInternalEndpoint
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+        private SenderFromCode mySender;
+        private ReceiverFromCode myReceiver;
+
+        public override void Run()
+        {
+            Trace.TraceInformation("RoleWithInternalEndpoint is running");
+
+            try
+            {
+                this.RunAsync(this.cancellationTokenSource.Token).Wait();
+            }
+            finally
+            {
+                this.runCompleteEvent.Set();
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // Setup communications with other roles
+            ConfigInterRoleComms();
+
+            bool result = base.OnStart();
+
+            Trace.TraceInformation("RoleWithInternalEndpoint has been started");
+
+            return result;
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("RoleWithInternalEndpoint is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("RoleWithInternalEndpoint has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following with your own logic.
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                
+                mySender.SaySomething("Role " + RoleEnvironment.CurrentRoleInstance.Id.ToString() + " says Hello World!");
+                Trace.TraceInformation("Working");
+                await Task.Delay(1000);
+            }
+        }
+
+        private bool ConfigInterRoleComms()
+        {
+            try
+            {
+
+                // Configure receiver
+                myReceiver = new ReceiverFromCode();
+                myReceiver.myAddress = RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["internalEndpoint"].IPEndpoint.ToString();
+                myReceiver.ConfigComms();
+
+                // Configure sender
+                mySender = new SenderFromCode();
+                mySender.myAddress = RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["internalEndpoint"].IPEndpoint.ToString();
+                return mySender.ConfigComms();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error configuring communication: " + ex.Message);
+                throw;
+            }
+
+        }
+    }
+}
