790de5ab5e91d854d381a8dda5f41df480a10942
Jim Lane <jimlane@microsoft.com>
2015-10-30 15:56:37 -0400

    Fix missing files

---
+++ InterRoleCommunication/RoleWithInternalEndpoint/ReceiverProxy.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.ServiceModel;
+
+namespace RoleWithInternalEndpoint
+{
+    [ServiceBehavior]
+    class ReceiverProxy : IConversation
+    {
+        public string Conversation(string Message)
+        {
+            Trace.TraceInformation("Received: " + Message);
+            return "You said: " + Message;
+        }
+    }
+
+}
